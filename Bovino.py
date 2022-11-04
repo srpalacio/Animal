b5e5921a49f960a690e7ae11524fb155ab98f620
srpalacio <112731430+srpalacio@users.noreply.github.com>
2022-11-04 17:40:18 -0500

    bbb

--- Bovino.py
+++ Bovino.py
@@ -3,13 +3,20 @@ from Animal import Animal
 class Bovino:
 
     def __init__(self):
-        self.propietario
-        self.fechaVacunacion
+
         self.establo
 
     def pastar(self):
-        pass
+        
+        if self.establo<=5:
+            print("No pastar")
+        else:
+            print("Pastar")
 
+
+    def emitirSonido(self):
+        
+        print("Muu")
     
 
     
