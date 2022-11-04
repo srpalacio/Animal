e5197c302766a60dcd70a0be165445febceeffff
srpalacio <112731430+srpalacio@users.noreply.github.com>
2022-11-04 17:19:32 -0500

    Granja update 0.1

--- Granja.py
+++ Granja.py
@@ -7,8 +7,17 @@ class Granja:
         self.misPerros=[]
         self.misBovinos=[]
 
-    def agregarBovino(self):
-        pass
+    def agregarBovino(self,peso,edad,raza,propietario,fechaVacunacion,establo):
+        
+        miBovino=Bovino()
+        self.miBovino.peso=peso
+        self.miBovino.edad=edad
+        self.miBovino.raza=raza
+        self.miBovino.propietario=propietario
+        self.miBovino.fechaVacunacion=fechaVacunacion
+        self.miBovino.establo=establo
+        self.misBovinos.append(miBovino)
 
-    def obtenerBovino(self):
-        pass
+    def obtenerBovino(self,codigo):
+
+        return self.misBovinos[codigo]
