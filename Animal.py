1ff10e9e493bd4d92fd2196f6d8cb3262cfdfd34
srpalacio <112731430+srpalacio@users.noreply.github.com>
2022-11-04 16:36:39 -0500

    v0.0.1

--- Animal.py
+++ Animal.py
@@ -1,7 +1,30 @@
+
+
 class Animal:
+
     def __init__(self):
-        
+
         self.peso
+        self.edad
+        self.raza
+
+    def correr(self):
+        
+        if self.edad>=5:
+            print("Rápido")
+        else:
+            print("Lento")
+
+    def emitirSonido(self):
+        
+        if self.edad>=3:
+            print("Guau Guau")
+        else:
+            print("Auf Auf")
+
+    def obtenerEdad(self):
+        
+        print(self.edad)
         
 
 
