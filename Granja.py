a3b727e9ccc411740c3cabfff9a767e537d3d1af
srpalacio <112731430+srpalacio@users.noreply.github.com>
2022-11-04 16:37:18 -0500

    v0.0.2

--- Granja.py
+++ Granja.py
@@ -1,3 +1,15 @@
+
+
 class Granja:
+
     def __init__(self):
-        
+        self.perro=[]
+        self.bovino=[]
+        self.miPerros=len(self.perro)
+        self.misBovinos=len(self.bovino)
+
+    def agregarBovino(self):
+        pass
+
+    def obtenerBovino(self):
+        pass
